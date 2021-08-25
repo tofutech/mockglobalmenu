175c8a95251957a28e235a7c9392ecffb3b37919
tofutech <81934761+tofutech@users.noreply.github.com>
2021-08-24 23:45:28 -0400

    Add files via upload

---
+++ mockglobalmenu@tofutech/extension.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { St, Clutter } = imports.gi;
+
+const Main = imports.ui.main;
+
+let _myText;
+
+class Extension {
+
+    enable() {
+    const _myText = new St.Label({ text: '  File        Edit        View        Go        Window        Help', y_align: Clutter.ActorAlign.CENTER, style_class: 'panel-button', track_hover: false, reactive: false, style_class: 'panel-button my-class'});
+    Main.panel._leftBox.insert_child_at_index(_myText, 10)
+    }
+
+    disable() {
+    _myText.destroy();
+    }
+}
+
+function init() {
+    return new Extension();
+}
